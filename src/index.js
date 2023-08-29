83346f66d6c0a079f75d4e97d235a373f4dc8f76
unknown <shishkina.ja@gmail.com>
2023-08-29 21:08:11 +0200

    feat: not complete

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ module.exports = function check(str, bracketsConfig) {
   for(let i=0; i< str.length; i++) {
     let currItem = str[i]
     bracketsConfig.forEach((val, ind)=> {
-      if(currItem === val[0]){
+      if(currItem === val[0] && currItem !== val[1]){
         arr.push(currItem)
       } else if(currItem === val[1] )  { //&& bracketsConfig[ind][1]=== val[1]
         let lastItemInArr = arr[arr.length-1]
@@ -20,27 +20,3 @@ module.exports = function check(str, bracketsConfig) {
   }
   return arr.length === 0 ? true : false
 }
-  // console.log(bracketsConfig)
-  // console.log(str.includes(bracketsConfig))
-  // str = str.replaceAll(...bracketsConfig, '')
-  // console.log(str)
-  // let strIncludesConf = 0
-  // function checkStr() {
-  //   for(let i =0; i<bracketsConfig.length; i++) {
-  //     if(str.includes(bracketsConfig[i])){
-  //       strIncludesConf = 1
-  //     } else {
-  //     strIncludesConf=0
-  //     } 
-  //   }
-  // }
-  // do  {
-  //     checkStr()
-  //     if(strIncludesConf == 1) {
-  //       str = str.replaceAll(...bracketsConfig, '')
-  //     }
-  // } while (strIncludesConf == 1)
-  
-  // console.log(strIncludesConf)
-
-  // return str.length == 0? true: false
